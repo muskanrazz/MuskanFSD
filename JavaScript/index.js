e88f1c7ab3f3423083f14e53c227fdf9d4d4413d
Muskan kumari <muskanrazz006@gmail.com>
2026-05-16 20:34:46 +0530

    basic_javascript

---
+++ JavaScript/index.js
@@ -0,0 +1,19 @@
+console.log('Hello World')
+console.log(2)
+console.log(true)
+console.log('Muskan')
+console.log("Kumari")
+console.log(`bhopal`)
+a="muskan"
+b="kumari"
+console.log(a + " " +b)
+console.log(""==0) //it's true
+
+
+const c= true
+if(c==true) {
+    const a=14;
+    console.log(a)
+}
+
+else  console.log('bye')
---
+++ JavaScript/var.js
@@ -0,0 +1,7 @@
+const a= 'muskan'
+const b= 26
+const c= true
+const d= {}
+const e= []
+
+console.log(typeof b)
